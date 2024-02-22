456c1292953ecd9e5718270a242e025234a511ce
Aeron Red Celajes <142370807+AeronRedCelajes@users.noreply.github.com>
2024-02-22 02:00:11 +0000

    statePattern code here

--- statePattern/ClosedState.java
+++ statePattern/ClosedState.java
@@ -5,11 +5,13 @@ public class ClosedState implements AccountState{
     @Override
     public void deposit(Account account, Double amount) {
         System.out.println("You cannot deposit a closed account!");
+        account.toString();
     }
 
     @Override
     public void withdraw(Account account, Double amount) {
         System.out.println("You cannot withdraw a closed account!");
+        account.toString();
     }
 
     @Override
